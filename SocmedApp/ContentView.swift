d45dbb3e6817376f8cfe50d35c5fd9402a2b615f
nululalbab <nululalbab@gmail.com>
2020-09-18 21:10:56 +0700

    Initial Commit

---
+++ SocmedApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocmedApp
+//
+//  Created by Najibullah Ulul Albab on 18/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocmedApp/SocmedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocmedAppApp.swift
+//  SocmedApp
+//
+//  Created by Najibullah Ulul Albab on 18/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SocmedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
